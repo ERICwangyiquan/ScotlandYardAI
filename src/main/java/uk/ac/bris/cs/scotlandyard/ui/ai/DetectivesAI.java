cebaa0a44be26ac429d9391670bdc4d9299d8144
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2023-04-10 10:59:12 +0100

    fix: DetectiveAI to correctly use `ItNegamax`

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/DetectivesAI.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/DetectivesAI.java
@@ -33,16 +33,37 @@ public class DetectivesAI implements Ai {
 				.filter((Optional<Integer> l) -> l.isPresent())
 				.reduce((fst, snd) -> snd) // take last element of (finite) stream
 				.orElse(Optional.empty());
-		Function<Move, Double> score = (Move m) -> gameTree.ItNegamax(
+		BiFunction<Integer, Move, Double> score = (Integer d, Move m) -> gameTree.ItNegamax(
 				new ImmutableGameState(board, mrXLocation.orElse(1)).clone().advance(m),
-				3, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, mrXLocation, startTime,
-				timeoutPair);
-		// throws when getAvailableMoves is empty
-		return board.getAvailableMoves()
-				.stream()
-				.parallel()
-				.min(Comparator.comparing(m -> score.apply(m)))
-				.get();
+				3, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, mrXLocation, startTime, timeoutPair);
+		Move move = null;
+		for (Integer d = 0; d < 3; d++) {
+			// BUG?
+			// Haven't tested, but since it's the same as in MrXAI... it's probably too slow
+			// and the iterative deepening implementation is probably shoddy.
+
+			// Think I've just misunderstood how to make use of it? The idea is
+			// to use iterative depening to order the moves via a transposition
+			// table, however I now don't think I really understood how to make
+			// that work -- that would mean the sorting is useless and so
+			// there's not enough pruning. The main way in which we can improve runtime
+			// is with better algorithms and heuristics, in particular pruning. We can use
+			// parallel programming here, but overusing it is a bit useless...
+
+			// We also need to find a way to make proper use of time management
+			// AT THE SAME TIME as parallel programming -- the use of iterative
+			// deepening should help in a similar way and might be intertwined.
+
+			// due to Java loop mechanics, d is not final, but we need finally typed value
+			final Integer depth = d;
+			move = board
+					.getAvailableMoves()
+					.stream()
+					.parallel()
+					.min(Comparator.comparingDouble(m -> score.apply(depth, m)))
+					.get();
+		}
+		return move;
 	}
 
 	@Override
