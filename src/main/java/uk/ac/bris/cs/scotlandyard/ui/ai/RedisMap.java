58605dd697e63a6a1adfb8a8660f64563532e5d5
ERICwangyiquan <ericwangyiquan@gmail.com>
2023-05-03 11:59:27 +0100

    small change

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/RedisMap.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/RedisMap.java
@@ -7,7 +7,7 @@ public class RedisMap {
     public static void createTable(ImmutableGameState gameState) {
         //create a table in Redis if it's not existed
         try (Jedis jedis = new Jedis("redis://localhost:6379")){
-            if (jedis.hvals(key).size() == 0) {
+            if (jedis.hvals(key).size() != 39601) {
                 for (int start : gameState.getSetup().graph.nodes()) {
                     Dijkstra dijkstra = new Dijkstra(gameState, start);
                     for (int end : gameState.getSetup().graph.nodes()) {
